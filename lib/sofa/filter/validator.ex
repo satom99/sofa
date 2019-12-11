ab38ce154af39c0d737c0263758e73976cbcfd85
Santiago Tortosa <santiago.tortosa@azertiumit.com>
2019-12-11 11:09:53 +0100

    Filter validation tweaks

--- lib/sofa/filter/validator.ex
+++ lib/sofa/filter/validator.ex
@@ -30,6 +30,7 @@ defmodule Sofa.Filter.Validator do
     defp validate_operation(changeset, object) do
         changeset
         |> get_change(:op)
+        |> to_string
         |> operate(changeset, object)
     end
     defp operate("defined", changeset, _object) do
@@ -65,8 +66,8 @@ defmodule Sofa.Filter.Validator do
     defp operate("any", changeset, object) do
         reduce(changeset, :map, object)
     end
-    defp operate(_code, changeset, _object) do
-        add_error(changeset, :op, "unknown")
+    defp operate(code, changeset, _object) do
+        add_error(changeset, :op, "unknown operation #{code}")
     end
 
     defp insert(changeset, object) do
