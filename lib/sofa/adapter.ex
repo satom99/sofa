6681afec003c56a74336f3e2ae043b3fea3b9d08
Santiago Tortosa <santiago.tortosa@azertiumit.com>
2020-01-18 04:28:13 +0100

    Select field name uniqueness madness

--- lib/sofa/adapter.ex
+++ lib/sofa/adapter.ex
@@ -5,6 +5,11 @@ defmodule Ecto.Adapters.Couchbase do
         driver: :couchbase,
         migration_lock: nil
     ]
+
+    import Sofa.Builder
+
+    alias Ecto.Query
+    alias Ecto.Query.SelectExpr
     alias Sofa.Request
 
     def supports_ddl_transaction? do
@@ -12,23 +17,8 @@ defmodule Ecto.Adapters.Couchbase do
     end
 
     def prepare(operation, query) do
-        signature = query
-        |> Map.get(:select)
-        |> Map.get(:fields)
-        |> Enum.map(&field/1)
-
-        fields = query
-        |> Map.get(:select)
-        |> Map.get(:fields)
-        |> Enum.uniq_by(&uniqueness/1)
-
-        select = query
-        |> Map.get(:select)
-        |> Map.put(:fields, fields)
-
-        query = query
-        |> Map.put(:select, select)
-        |> Map.put(:tampered, true)
+        query = aliasing(query)
+        signature = signature(query)
 
         statement = @conn
         |> apply(operation, [query])
@@ -45,6 +35,7 @@ defmodule Ecto.Adapters.Couchbase do
         params = primarize(params)
         super(adapter, schema, params, conflict, returning, options)
     end
+
     def insert_all(adapter, schema, header, params, conflict, returning, options) do
         header = [:id | header]
         params = Enum.map(params, &primarize/1)
@@ -57,20 +48,74 @@ defmodule Ecto.Adapters.Couchbase do
         |> Enum.concat(params)
     end
 
-    defp field(expression) do
-        expression
-        |> expand
-        |> List.last
+    defp aliasing(%Query{select: nil} = query) do
+        query
     end
-    
-    defp uniqueness(expression) do
-        expand(expression)
+    defp aliasing(%Query{select: select} = query) do
+        select = aliasing(select)
+        %{query | select: select}
+    end
+    defp aliasing(%SelectExpr{fields: fields} = select) do
+        fields = aliasing(fields)
+        %{select | fields: fields}
+    end
+    defp aliasing(expressions) when is_list(expressions) do
+        expressions
+        |> Enum.map(&aliasing/1)
+        |> Enum.reduce({[], []}, &aliasing/2)
+        |> Kernel.elem(1)
+        |> Enum.reverse
+    end
+    defp aliasing({:fragment, _context, _options} = expression) do
+        {reference(), expression}
+    end
+    defp aliasing(expression) do
+        expression
+    end
+    defp aliasing(expression, {uniques, expressions}) do
+        unique = uniqueness(expression)
+        expression = unless unique in uniques do
+            expression
+        else
+            {reference(), expression}
+        end
+        {[unique | uniques], [expression | expressions]}
     end
 
-    defp expand({:., _context, arguments}) do
-        arguments
+    defp aliasing(expression, processed) do
+        uniqueness = uniqueness(expression)
+        IO.inspect {uniqueness, processed}
+        expression = unless uniqueness in processed do
+            expression
+        else
+            {reference(), expression}
+        end
+        [expression | processed]
     end
-    defp expand({tuple, _context, _arguments}) do
-        expand(tuple)
+
+    defp signature(%Query{select: nil}) do
+        []
+    end
+    defp signature(%Query{select: select}) do
+        select
+        |> Map.get(:fields)
+        |> Enum.map(&signature/1)
+        |> Enum.map(&to_string/1)
+    end
+    defp signature({alias, _expression}) do
+        alias
+    end
+    defp signature(expression) do
+        uniqueness(expression)
+    end
+
+    defp uniqueness({tuple, _context, _arguments}) when is_tuple(tuple) do
+        uniqueness(tuple)
+    end
+    defp uniqueness({:., _context, arguments}) do
+        List.last(arguments)
+    end
+    defp uniqueness(expression) do
+        expression
     end
 end
